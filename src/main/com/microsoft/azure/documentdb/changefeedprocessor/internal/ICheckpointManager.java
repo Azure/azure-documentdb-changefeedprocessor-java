1259b3f11f49b9029dcb2ee75353718ed9335c51
Fabricio Catae <fcatae@microsoft.com>
2017-09-19 13:47:52 -0700

    add ICheckpointmgr

--- src/main/com/microsoft/azure/documentdb/changefeedprocessor/internal/ICheckpointManager.java
+++ src/main/com/microsoft/azure/documentdb/changefeedprocessor/internal/ICheckpointManager.java
@@ -7,26 +7,17 @@ package com.microsoft.azure.documentdb.changefeedprocessor.internal;
 
 /**
  *
- * @author yoterada
+ * @author fcatae
  */
+/// <summary>
+/// Provides methods for running checkpoint asynchronously. Extensibility is provided to specify host-specific storage for storing the offset.
+/// </summary>
 public interface ICheckpointManager {
-    /// <summary>
-    /// Provides methods for running checkpoint asynchronously. Extensibility is provided to specify host-specific storage for storing the offset.
-    /// </summary>
 
-    interface ICheckpointManager
-
-    {
-
-        /// <summary>Stores the offset of a particular partition in the host-specific store.</summary>
-
-        /// <param name="lease">Partition information against which to perform a checkpoint.</param>
-
-        /// <param name="offset">Current position in the stream.</param>
-
-        /// <param name="sequenceNumber">The sequence number of the partition.</param>
-
-        /// <returns>Returns <see cref="System.Threading.Tasks.Task" />.</returns>
-
-        Task<Lease> CheckpointAsync(Lease lease, string offset, long sequenceNumber);
+    /// <summary>Stores the offset of a particular partition in the host-specific store.</summary>
+    /// <param name="lease">Partition information against which to perform a checkpoint.</param>
+    /// <param name="offset">Current position in the stream.</param>
+    /// <param name="sequenceNumber">The sequence number of the partition.</param>
+    /// <returns>Returns <see cref="System.Threading.Tasks.Task" />.</returns>
+    Lease checkpoint(Lease lease, String offset, long sequenceNumber);
 }
