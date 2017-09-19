104249dc6fdb0a98083cfdd36d7642ce99ff777f
Fabricio Catae <fcatae@microsoft.com>
2017-09-19 13:24:13 -0700

    fix icheckpoint

--- src/main/com/microsoft/azure/documentdb/changefeedprocessor/internal/ICheckpointManager.java
+++ src/main/com/microsoft/azure/documentdb/changefeedprocessor/internal/ICheckpointManager.java
@@ -14,19 +14,4 @@ public interface ICheckpointManager {
     /// Provides methods for running checkpoint asynchronously. Extensibility is provided to specify host-specific storage for storing the offset.
     /// </summary>
 
-    interface ICheckpointManager
-
-    {
-
-        /// <summary>Stores the offset of a particular partition in the host-specific store.</summary>
-
-        /// <param name="lease">Partition information against which to perform a checkpoint.</param>
-
-        /// <param name="offset">Current position in the stream.</param>
-
-        /// <param name="sequenceNumber">The sequence number of the partition.</param>
-
-        /// <returns>Returns <see cref="System.Threading.Tasks.Task" />.</returns>
-
-        Task<Lease> CheckpointAsync(Lease lease, string offset, long sequenceNumber);
 }
