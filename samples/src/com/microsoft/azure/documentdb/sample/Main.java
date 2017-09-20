75aecb6a66643623c481c0fc8ad9be93615e86cb
Ryan CrawCour <ryancraw@microsoft.com>
2017-09-20 15:48:19 -0700

    sample app scaffold

---
+++ samples/src/com/microsoft/azure/documentdb/sample/Main.java
@@ -0,0 +1,9 @@
+package com.microsoft.azure.documentdb.sample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("foo");
+    }
+}
