a3e16ac5e71ac83ee72a11f0c5895bac2f632ae4
ViniciusSouza <visouza@microsoft.com>
2017-09-20 14:04:10 -0300

    Adding AppSecrets and tests

--- src/test/com/microsoft/azure/documentdb/changefeedprocessor/ChangeFeedEventHostTest.java
+++ src/test/com/microsoft/azure/documentdb/changefeedprocessor/ChangeFeedEventHostTest.java
@@ -1,7 +1,12 @@
 package com.microsoft.azure.documentdb.changefeedprocessor;
 
 import com.microsoft.azure.documentdb.ChangeFeedOptions;
+import com.microsoft.azure.documentdb.changefeedprocessor.internal.ConfigurationException;
+import com.microsoft.azure.documentdb.changefeedprocessor.internal.ConfigurationFile;
 import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class ChangeFeedEventHostTest {
 
@@ -34,5 +39,22 @@ public class ChangeFeedEventHostTest {
         ChangeFeedEventHost host = new ChangeFeedEventHost(
                 "test",
                 new DocumentCollectionInfo(), new DocumentCollectionInfo(), new ChangeFeedOptions(), null);
+    }
+
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testCreatChangeFeedHostUsingSecrets() throws ConfigurationException, URISyntaxException {
+        ConfigurationFile config = new ConfigurationFile("app.secrets");
+
+        DocumentCollectionInfo docInfo = new DocumentCollectionInfo();
+        docInfo.setUri(new URI(config.get("COSMOSDB_ENDPOINT")));
+        docInfo.setMasterKey(config.get("COSMOSDB_SECRET"));
+        docInfo.setDatabaseName(config.get("COSMOSDB_DATABASE"));
+        docInfo.setCollectionName(config.get("COSMOSDB_COLLECTION"));
+
+        DocumentCollectionInfo docAux = new DocumentCollectionInfo(docInfo);
+        docAux.setCollectionName(config.get("COSMOSDB_AUX_COLLECTION"));
+
+        ChangeFeedEventHost host = new ChangeFeedEventHost("hotsname", docInfo, docAux );
     }
 }
