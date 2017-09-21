08a09b977c0c97957fbabfc1f51a060b13cece41
Fabricio Catae <fcatae@microsoft.com>
2017-09-21 13:57:58 -0700

    add checkpointStats

--- src/main/com/microsoft/azure/documentdb/changefeedprocessor/services/CheckpointServices.java
+++ src/main/com/microsoft/azure/documentdb/changefeedprocessor/services/CheckpointServices.java
@@ -1,5 +1,8 @@
 package com.microsoft.azure.documentdb.changefeedprocessor.services;
 
+import com.microsoft.azure.documentdb.changefeedprocessor.CheckpointStats;
+import com.microsoft.azure.documentdb.changefeedprocessor.internal.documentleasestore.DocumentServiceLease;
+
 import java.util.Dictionary;
 import java.util.Hashtable;
 
@@ -18,5 +21,39 @@ public class CheckpointServices {
 
     public void setCheckpointData(String partitionId, Object data) {
         checkpoints.put(partitionId, (String)data);
+    }
+
+    public boolean IsCheckpointNeeded(DocumentServiceLease lease, CheckpointStats checkpointStats)
+    {
+//        var options = new ChangeFeedHostOptions() { CheckpointFrequency = _checkpointOptions };
+//
+//        Debug.Assert(lease != null);
+//        Debug.Assert(checkpointStats != null);
+//
+//        if (checkpointStats.ProcessedDocCount == 0)
+//        {
+//            return false;
+//        }
+
+        boolean isCheckpointNeeded = true;
+
+//        if (options.CheckpointFrequency != null &&
+//                (options.CheckpointFrequency.ProcessedDocumentCount.HasValue || options.CheckpointFrequency.TimeInterval.HasValue))
+//        {
+//            // Note: if either condition is satisfied, we checkpoint.
+//            isCheckpointNeeded = false;
+//            if (options.CheckpointFrequency.ProcessedDocumentCount.HasValue)
+//            {
+//                isCheckpointNeeded = checkpointStats.ProcessedDocCount >= options.CheckpointFrequency.ProcessedDocumentCount.Value;
+//            }
+//
+//            if (options.CheckpointFrequency.TimeInterval.HasValue)
+//            {
+//                isCheckpointNeeded = isCheckpointNeeded ||
+//                        DateTime.Now - checkpointStats.LastCheckpointTime >= options.CheckpointFrequency.TimeInterval.Value;
+//            }
+//        }
+
+        return isCheckpointNeeded;
     }
 }
