2a88d4197c3793f4b9ccf87cf2388f5eff806b5f
Fabricio Catae <fcatae@microsoft.com>
2017-09-26 17:49:45 -0700

    basic lease management

--- src/main/com/microsoft/azure/documentdb/changefeedprocessor/services/LeaseServices.java
+++ src/main/com/microsoft/azure/documentdb/changefeedprocessor/services/LeaseServices.java
@@ -1,43 +1,74 @@
 package com.microsoft.azure.documentdb.changefeedprocessor.services;
 
 public abstract class LeaseServices {
-    private ResourcePartitionCollection partitions;
+    private ResourcePartitionCollection availablePartitions;
+    private ResourcePartitionCollection leasedPartitions;
     private ILeaseSubscriber subscriber;
 
     public LeaseServices() {
-        this.partitions = new ResourcePartitionCollection();
+        this.availablePartitions = new ResourcePartitionCollection();
+        this.leasedPartitions = new ResourcePartitionCollection();
         this.subscriber = null;
     }
 
     public void register(ResourcePartition partition) {
-        partitions.put(partition);
+        availablePartitions.put(partition);
+
+        // todo: verify partition does not exist in Lease collection
     }
 
     public void unregister(ResourcePartition partition) {
-        partitions.remove(partition);
+        availablePartitions.remove(partition);
+
+        // todo: verify partition is in available state
     }
 
     public void subscribe(ILeaseSubscriber subscriber) {
         this.subscriber = subscriber;
     }
 
-    public void acquire(String partitionId) {
-        if( subscriber != null ) {
-            ResourcePartition partition = partitions.get(partitionId);
+    protected void acquire(String partitionId) {
+        ResourcePartition partition = availablePartitions.get(partitionId);
 
-            if(partition != null) {
-                subscriber.onLeaseAcquired(partition);
-            }
+        if(partition != null) {
+            notifyAcquire(partition);
+            changePartitionToLease(partition);
         }
     }
 
-    public void release(String partitionId) {
-        if( subscriber != null ) {
-            ResourcePartition partition = partitions.get(partitionId);
+    protected void release(String partitionId) {
+        ResourcePartition partition = leasedPartitions.get(partitionId);
 
-            if(partition != null) {
-                subscriber.onLeaseReleased(partition);
-            }
+        if(partition != null) {
+            changePartitionToAvailable(partition);
+            notifyRelease(partition);
         }
     }
+
+    private void notifyAcquire(ResourcePartition partition) {
+        if( (subscriber != null) && (partition != null)) {
+            subscriber.onLeaseAcquired(partition);
+        }
+    }
+
+    private void notifyRelease(ResourcePartition partition) {
+        if( (subscriber != null) && (partition != null)) {
+            subscriber.onLeaseReleased(partition);
+        }
+    }
+
+    private void changePartitionToLease(ResourcePartition partition) {
+        synchronized (leasedPartitions) {
+            availablePartitions.remove(partition);
+            leasedPartitions.put(partition);
+        }
+    }
+
+    private void changePartitionToAvailable(ResourcePartition partition) {
+        synchronized (leasedPartitions) {
+            leasedPartitions.remove(partition);
+            availablePartitions.put(partition);
+        }
+    }
+
 }
--- src/test/com/microsoft/azure/documentdb/changefeedprocessor/services/TestLeaseServices.java
+++ src/test/com/microsoft/azure/documentdb/changefeedprocessor/services/TestLeaseServices.java
@@ -1,14 +1,30 @@
 package com.microsoft.azure.documentdb.changefeedprocessor.services;
 
 public class TestLeaseServices extends LeaseServices {
-//    @Override
-//    public void register(ResourcePartition partition) {
-//        System.out.println("registered partition: " + partition.getId());
-//    }
-//
-//    @Override
-//    public void unregister(ResourcePartition partition) {
-//        System.out.println("UNregistered partition: " + partition.getId());
-//    }
 
+    @Override
+    public void register(ResourcePartition partition) {
+        // register first
+        super.register(partition);
+        // immediately acquire
+        acquire(partition.getId());
+    }
+
+    @Override
+    public void unregister(ResourcePartition partition) {
+        // immediately release
+        release(partition.getId());
+        // then unregister
+        super.unregister(partition);
+    }
+
+    @Override
+    public void acquire(String partitionId) {
+        super.acquire(partitionId);
+    }
+
+    @Override
+    public void release(String partitionId) {
+        super.release(partitionId);
+    }
 }
